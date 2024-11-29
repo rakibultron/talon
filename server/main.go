dde80f5719510fe1d5431bc4e74ca93236c2b268
rakibultron <mdrakibul.dev@gmail.com>
2024-11-30 01:21:11 +0600

    chore: add goose for managing database migrations, and cobra with makefile for managing app commands

--- server/main.go
+++ server/main.go
@@ -1,13 +1,60 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rakibultron/talon/db"
+	"github.com/spf13/cobra"
+)
 
 func main() {
+
+	// Load environment variables
+	envErr := godotenv.Load(".env.local")
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
+	// Create the root command
+	var rootCmd = &cobra.Command{
+		Use:   "talon",
+		Short: "talon API Application",
+		Long:  "talon API Application with support for embedded migrations and flexible commands.",
+		Run: func(cmd *cobra.Command, args []string) {
+			startServer(port)
+		},
+	}
+
+	// Add migration commands
+	rootCmd.AddCommand(db.MigrateCommand)
+
+	// Execute the CLI
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startServer(port string) {
+	// Initialize Gin router
 	r := gin.Default()
+
+	// Define basic ping route
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	// Start the server
+	fmt.Printf("Starting server on port %s\n", port)
+	r.Run(":" + port)
 }
